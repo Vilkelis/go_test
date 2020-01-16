3bfa82e4e529d2bfdb7ca77d55b7452240c6fef0
Stepan Golovanov <vilkelis@yandex.ru>
2020-01-16 20:45:30 +0500

    Added 3 git add .! on v2

--- test_mod.go
+++ test_mod.go
@@ -9,14 +9,14 @@ import (
 func Hi(name, lang string) (string, error) {
     switch lang {
     case "en":
-        return fmt.Sprintf("Hi, %s!", name), nil
+        return fmt.Sprintf("Hi, %s!!!", name), nil
     case "pt":
-        return fmt.Sprintf("Oi, %s!", name), nil
+        return fmt.Sprintf("Oi, %s!!!", name), nil
     case "es":
-        return fmt.Sprintf("¡Hola, %s!", name), nil
+        return fmt.Sprintf("¡Hola, %s!!!", name), nil
     case "fr":
-        return fmt.Sprintf("Bonjour, %s!", name), nil
+        return fmt.Sprintf("Bonjour, %s!!!", name), nil
     default:
-        return "", errors.New("unknown language")
+        return "", errors.New("unknown language!!!")
     }
 }
